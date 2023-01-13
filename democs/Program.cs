d0042f2421fce7e45ff5abca908dfbadbcf81c5d
phithuan <turnthuan1@gmail.com>
2023-01-13 18:05:43 +0700

    the first commit

---
+++ democs/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace baitaptinhso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int A, B;
+          int tong, hieu, tich;
+          double thuong;
+          string strA, strB;
+          bool xt1, xt2;
+
+          Console.Write("nhap so A: ");
+          strA = Console.ReadLine();
+          Console.Write("nhap so B: ");
+          strB = Console.ReadLine();
+
+          xt1 = int.TryParse(strA, out A);
+          Console.Write("So a nhap vao la: " ); Console.WriteLine(xt1 == true ? "dung" : "sai");
+          xt2 = int.TryParse(strB, out B);
+          Console.Write("So b nhap vao la: " ); Console.WriteLine(xt2 == true ? "dung" : "sai");
+          
+          tong = A+B;
+          hieu = A-B;
+          tich = A*B;
+          thuong = (double)A/B;
+
+          Console.WriteLine(" Tong = " + tong);
+          Console.WriteLine(" Hieu = " + hieu);
+          Console.WriteLine(" Tich = " + tich);
+          Console.WriteLine(" Thuong = " + thuong);
+        }
+    }
+}
